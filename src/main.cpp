b0beae18338347fe0890ca3659bf40243a084ba4
Reimanbow <reimanbow0318@gmail.com>
2024-07-31 18:03:28 +0900

    add: send MQTT

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <Arduino.h>
 #include <M5Unified.h>
 #include <Wire.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
 
 M5Canvas canvas(&M5.Display);
 
@@ -11,6 +14,50 @@ SensirionI2CScd4x scd40;
 // SEN55
 #include <SensirionI2CSen5x.h>
 SensirionI2CSen5x sen55;
+
+// WiFi
+const char* ssid = "";
+const char* pass = "";
+const char* server_addr = "";
+const int server_port = 1883;
+const char* device_ID = "AirQ";
+const char* pub_topic = "/env";
+
+WiFiClient client;
+PubSubClient mqttClient(client);
+
+void connectMqtt(){
+  while(!mqttClient.connected()){
+    if(mqttClient.connect(device_ID)){
+      canvas.fillScreen(BLACK);
+      canvas.setCursor(0, 0);
+      canvas.print("environment sensing");
+      canvas.pushSprite(0, 0);
+    }else{
+      canvas.fillScreen(BLACK);
+      canvas.setCursor(0, 0);
+      canvas.print("not connect MQTT");
+      canvas.pushSprite(0, 0);
+      delay(500);
+    }
+  }
+}
+
+void mqttLoop(){
+  if(!mqttClient.connected()){
+    connectMqtt();
+  }
+  mqttClient.loop();
+}
+
+void errorHandling(uint16_t error){
+  canvas.clearDisplay();
+  canvas.setCursor(0, 0);
+  char errorMessage[256];
+  errorToString(error, errorMessage, 256);
+  canvas.println(errorMessage);
+  canvas.pushSprite(0, 0);
+}
 
 void setup(){
   auto cfg = M5.config();
@@ -34,10 +81,8 @@ void setup(){
   error = scd40.stopPeriodicMeasurement();
   
   if(error){
-    errorToString(error, errorMessage, 256);
-    canvas.printf("%s", errorMessage);
-    canvas.pushSprite(0, 0);
-    delay(1000);
+    errorHandling(error);
+    return;
   }
 
   uint16_t serial0;
@@ -48,46 +93,51 @@ void setup(){
   error = scd40.startPeriodicMeasurement();
   
   if(error){
-    errorToString(error, errorMessage, 256);
-    canvas.printf("%s", errorMessage);
-    canvas.pushSprite(0, 0);
-    delay(1000);
+    errorHandling(error);
+    return;
   }
 
   // SEN55
   sen55.begin(Wire);
   error = sen55.deviceReset();
   if(error){
-    errorToString(error, errorMessage, 256);
-    canvas.printf("%s", errorMessage);
-    canvas.pushSprite(0, 0);
-    delay(1000);
+    errorHandling(error);
+    return;
   }
   float tempOffset = 0.0F;
   error = sen55.setTemperatureOffsetSimple(tempOffset);
   if(error){
-    errorToString(error, errorMessage, 256);
-    canvas.printf("%s", errorMessage);
-    canvas.pushSprite(0, 0);
-    delay(1000);
+    errorHandling(error);
+    return;
   }
+
+  // WiFi
+  WiFi.begin(ssid, pass);
+  while(WiFi.status() != WL_CONNECTED){
+    canvas.fillScreen(BLACK);
+    canvas.setCursor(0, 0);
+    canvas.print("not connect WiFi");
+    canvas.pushSprite(0, 0);
+    delay(500);
+  }
+  mqttClient.setServer(server_addr, server_port);
+  
+  // start SEN55
   error = sen55.startMeasurement();
   if(error){
-    errorToString(error, errorMessage, 256);
-    canvas.printf("%s", errorMessage);
-    canvas.pushSprite(0, 0);
-    delay(1000);
+    errorHandling(error);
+    return;
   }
 }
 
 // 1s周期でデータを取得する
 void loop(){
+  mqttLoop();
   unsigned long start_ms = millis();
-  canvas.clearDisplay();
-  canvas.setCursor(0, 0);
+  // canvas.clearDisplay();
+  // canvas.setCursor(0, 0);
 
   uint16_t error;
-  char errorMessage[256];
 
   uint16_t co2 = 0;
   float temperature = 0.0f;
@@ -95,19 +145,22 @@ void loop(){
   bool isDataReady = false;
   error = scd40.getDataReadyFlag(isDataReady);
   if(error){
+    //errorHandling(error);
     return;
   }
   if(!isDataReady){
+    //errorHandling(error);
     return;
   }
   error = scd40.readMeasurement(co2, temperature, humidity);
   if(error){
+    //errorHandling(error);
     return;
   }
-  canvas.printf("SCD40\n");
-  canvas.printf("CO2: %d\n", co2);
-  canvas.printf("Temperature: %f\n", temperature);
-  canvas.printf("Humidity: %f\n\n", humidity);
+  // canvas.printf("SCD40\n");
+  // canvas.printf("CO2: %d\n", co2);
+  // canvas.printf("Temperature: %f\n", temperature);
+  // canvas.printf("Humidity: %f\n\n", humidity);
 
   // SEN55
   float massConcentrationPm1p0;
@@ -124,23 +177,38 @@ void loop(){
     massConcentrationPm10p0, ambientHumidity, ambientTemperature, vocIndex,
     noxIndex);
   if(error){
-    canvas.println("Error trying to execute readMeasuredValues(): ");
-    errorToString(error, errorMessage, 256);
-    canvas.println(errorMessage);
-    canvas.pushSprite(0, 0);
+    //errorHandling(error);
     return;
   }
 
-  canvas.printf("SEN55\n");
-  canvas.printf("PM1.0: %f\n", massConcentrationPm1p0);
-  canvas.printf("PM2.5: %f\n", massConcentrationPm2p5);
-  canvas.printf("PM4.0: %f\n", massConcentrationPm4p0);
-  canvas.printf("PM10.0: %f\n", massConcentrationPm10p0);
-  canvas.printf("Humidity: %f\n", ambientHumidity);
-  canvas.printf("Temperature: %f\n", ambientTemperature);
-  canvas.printf("VOC: %f\n", vocIndex);
-  canvas.printf("NOx: %f\n", noxIndex);
+  float temp_avg = (temperature + ambientTemperature) / 2;
+  float humi_avg = (humidity + ambientHumidity) / 2;
 
-  canvas.pushSprite(0, 0);
+  // toJson
+  char json[400];
+  const size_t capacity = JSON_OBJECT_SIZE(20);
+  StaticJsonDocument<capacity> doc;
+  doc["CO2"] = co2;
+  doc["temperature"] = temp_avg;
+  doc["humidity"] = humi_avg;
+  doc["pm1.0"] = massConcentrationPm1p0;
+  doc["pm2.5"] = massConcentrationPm2p5;
+  doc["pm4.0"] = massConcentrationPm4p0;
+  doc["pm10.0"] = massConcentrationPm10p0;
+  doc["voc"] = vocIndex;
+  doc["nox"] = noxIndex;
+  serializeJson(doc, json);
+  mqttClient.publish(pub_topic, json);
+  // canvas.printf("SEN55\n");
+  // canvas.printf("PM1.0: %f\n", massConcentrationPm1p0);
+  // canvas.printf("PM2.5: %f\n", massConcentrationPm2p5);
+  // canvas.printf("PM4.0: %f\n", massConcentrationPm4p0);
+  // canvas.printf("PM10.0: %f\n", massConcentrationPm10p0);
+  // canvas.printf("Humidity: %f\n", ambientHumidity);
+  // canvas.printf("Temperature: %f\n", ambientTemperature);
+  // canvas.printf("VOC: %f\n", vocIndex);
+  // canvas.printf("NOx: %f\n", noxIndex);
+
+  // canvas.pushSprite(0, 0);
   while((millis() - start_ms) < 1000);
 }
